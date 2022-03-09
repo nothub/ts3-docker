076816a407e69d6cba1329bd6ee54287c399db86
nothub <48992448+nothub@users.noreply.github.com>
2022-03-09 22:19:14 +0100

    entrypoint cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env sh
+
 set -eux
 
-addgroup                                                        \
-      --gid "${PGID}"           `#Group id`                     \
-      --system                  `#Create a system group`        \
+addgroup                                                     \
+      --gid "${PGID}"        `#Group id`                     \
+      --system               `#Create a system group`        \
       teamspeak
 
-adduser                                                         \
-      --uid "${PUID}"           `#User id`                      \
-      --system                  `#Create a system user`         \
-      --ingroup teamspeak       `#Add user to existing group`   \
-      --disabled-password       `#Do not assign a password`     \
-      --no-create-home          `#Do not create home directory` \
+adduser                                                      \
+      --uid "${PUID}"        `#User id`                      \
+      --system               `#Create a system user`         \
+      --ingroup teamspeak    `#Add user to existing group`   \
+      --disabled-password    `#Do not assign a password`     \
+      --no-create-home       `#Do not create home directory` \
       teamspeak
 
 chown -R teamspeak:teamspeak /data
